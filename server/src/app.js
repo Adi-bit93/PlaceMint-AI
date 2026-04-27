757ddd2583af70f1c12b46f2ce8652f7f14413d4
Aditya Chavan <chavanaditya9325@gmail.com>
2026-04-27 10:40:26 +0530

    server setup done

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,28 @@
-console.log("Hi there");
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './.env'
+});
+
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('server is running');
+});
+
+const PORT = process.env.PORT ;
+
+console.log('PORT', PORT)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
+export default app;
