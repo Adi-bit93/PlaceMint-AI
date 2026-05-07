5f44c9d43ee7cea818e148d12782189b34737db3
Aditya Chavan <chavanaditya9325@gmail.com>
2026-05-07 12:51:28 +0530

    feat: add StudentProfile schema with academic info, skills, projects, certifications, experiences, and placement status

--- server/src/models/StudentProfile.js
+++ server/src/models/StudentProfile.js
@@ -43,3 +43,99 @@ const experienceSchema = new mongoose.Schema({
     isCurrent: { type: Boolean, default: false },
     description: { type: String, trim: true },
 }, { _id: true });
+
+// Main Schema
+const studentProfileSchema = new mongoose.Schema({
+    // Link to user
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true, // one profile per user
+        index: true,
+    },
+
+    // Academic Info
+    enrollmentNumber: {
+        type: String,
+        required: [true, 'Enrollment number is required' ],
+        unique: true,
+        trim: true,
+        uppercase: true,
+    },
+    branch:{
+        type: String,
+        required: [true, 'Branch is required' ], 
+        enum: {
+            values: ['CE', 'IT', 'CSE', 'ENTC', 'MECH', 'CIVIL', 'EE', 'OTHER'],
+            message: 'Invalid branch selected',
+        },
+    },
+
+    batch: {
+        type: Number,
+        required: [true, 'Batch year is required'],
+    },
+
+    cgpa:{
+        type:Number,
+        required: [true, 'CGPA is required'],
+        min: [0, 'CGPA cannot be less than 0'],
+        max: [10, 'CGPA cannot exceed 10']
+    },
+    activeBacklogs:{
+        type: Number,
+        default: 0,
+        min: [0, 'Active backlogs cannot be negative '],
+    },
+    totalBacklogs: {
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+
+    // Skill
+
+    skills: {
+        type: [String],
+        default: [],
+        set: (skills) => skills.map((s) => s.toLowerCase().trim()),
+    },
+
+    resume: {
+        url: {type: String, default: null},  // Cloudinary URL
+        publicId: {type: String, default: null },
+        uploadedAt: {type: Date, default:null },
+
+        parsedText :{type: String, default: null , select: false}, // raw extracted text(AI engine)
+    },
+
+    projects: {type: [projectSchema], default: []},
+    certifications: {type: [certificationSchema], defualt: []},
+    experiences: {type: [experienceSchema], default: []},
+    
+    // Social Links
+    links: {
+        github: {type: String, default: null } ,
+        linkedin: {type: String, default: null}, 
+        portfolio: { type: String, default: null },
+    },
+
+    // placement status
+
+    placementStatus: {
+        type: String, 
+        enum: ['not_placed', 'placed', 'opted_out'],
+        default: 'not_placed', 
+        index: true,
+    },
+
+    placedAt: {
+        company: {type: String, default: null},
+        role: {type: String, default: null },
+        ctc: {type: Number, default:null },
+        offerDate: {type: Date, default: null },
+    },
+    
+
+})
