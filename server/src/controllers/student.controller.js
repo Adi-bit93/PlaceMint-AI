a18b7ab8294b79f82a964137c00155c000cf0523
Aditya Chavan <chavanaditya9325@gmail.com>
2026-05-28 16:53:25 +0530

    feat: enhance getMyProfile and add uploadResume functionality for improved profile management

--- server/src/controllers/student.controller.js
+++ server/src/controllers/student.controller.js
@@ -11,20 +11,21 @@ export const getMyProfile = asyncHandler(async (req, res) => {
     const profile = await StudentProfile.findOne({ user: req.user.id })
         .populate('user', 'name email profilePhoto isEmailVerified createdAt');
 
-        if(!profile){
-            return sendSuccess(res, {
-                message: 'Profile not set up yet.',
-                data: { profile: null },
-            });
-        }
-
+    if (!profile) {
         return sendSuccess(res, {
-            message: 'Profile fetched successfully.',
-            data : { profile },
+            message: 'Profile not set up yet.',
+            data: { profile: null },
         });
+    }
+
+    return sendSuccess(res, {
+        message: 'Profile fetched successfully.',
+        data: { profile },
+    });
 });
 
-export const upsertMyProfile = asyncHandler(async(req, res) => {
+// Create or Update Profile in single QQuery with upsert
+export const upsertMyProfile = asyncHandler(async (req, res) => {
     const {
         enrollmentNumber, branch, batch, cgpa, activeBacklogs, totalBacklogs, skills, links,
     } = req.body;
@@ -33,7 +34,7 @@ export const upsertMyProfile = asyncHandler(async(req, res) => {
         { user: req.user.id },
         {
             $set: {
-                ...(enrollmentNumber && {enrollmentNumber }),
+                ...(enrollmentNumber && { enrollmentNumber }),
                 ...(branch && { branch }),
                 ...(batch && { batch }),
                 ...(cgpa !== undefined && { cgpa }),
@@ -43,7 +44,7 @@ export const upsertMyProfile = asyncHandler(async(req, res) => {
                 ...(links && { links }),
             },
         },
-        { new : true, upsert: true, runValidators: true, setDefaultsOnInsert: true}
+        { new: true, upsert: true, runValidators: true, setDefaultsOnInsert: true }
     ).populate('user', 'name email profilePhoto');
 
     logger.info(`Profile updated: student ${req.user.id}`);
@@ -54,3 +55,40 @@ export const upsertMyProfile = asyncHandler(async(req, res) => {
         data: { profile },
     });
 });
+
+// Upload resume
+
+export const uploadResume = asyncHandler(async (req, res) => {
+    if (!req.file) {
+        throw new AppError('No file uploaded. Please attach a pdf file.', 400);
+    }
+
+    // find existing profile
+    const profile = await StudentProfile.findOne({ user: req.user.id });
+
+    // Delete old file before saving the new one 
+    if (profile?.resume?.publicId) {
+        await deleFromCloudinary(profile.resume.publicId, 'raw');
+    }
+
+    const updatedProfile = await StudentProfile.findOneAndUpdate(
+        { user: req.user.id },
+        {
+            $set: {
+                'resume.url': req.file.path, // Cloudinary secure Url
+                'resume.publicId': req.file.filename, // Cloudinary public_id
+                'resume.uploadedAt': new Date(),
+            },
+        },
+        { new: true, upsert: true, setDefaultsOnInsert: true }
+    );
+    logger.info(`Resume uploaded: student ${req.user.id} → ${req.file.path}`);
+
+    return sendSuccess(res, {
+        message: 'Resume uploaded successfully.',
+        data: {
+            resume: updatedProfile.resume,
+            profileCompleteness: updatedProfile.profileCompleteness,
+        },
+    })
+})
