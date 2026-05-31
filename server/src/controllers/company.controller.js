9ceb953fcb844b876192fcbbbcc4ce565777201a
Aditya Chavan <chavanaditya9325@gmail.com>
2026-05-31 18:47:36 +0530

    feat: add logo upload functionality and enhance profile update response

--- server/src/controllers/company.controller.js
+++ server/src/controllers/company.controller.js
@@ -66,16 +66,80 @@ export const upsertMyProfile = asyncHandler(async (req, res) => {
             }
         },
         {
-            new: true, upsert: true, runValidators: true, setDefaultsOnInsert: true 
+            new: true, upsert: true, runValidators: true, setDefaultsOnInsert: true
         }
     ).populate('user', 'name email');
 
     logger.info(`Company Profile updated: ${req.user.id}`);
 
     return sendSuccess(res, {
-        message: existing 
+        message: existing
             ? 'profile updated successfully.'
             : 'Profile created successfully. Awaiting admin approval.',
         data: { profile },
     });
-}) 
+});
+
+// Upload Logo
+export const uploadLogo = asyncHandler(async (req, res) => {
+    if (!req.file) {
+        throw new AppError('No file uploaded. Please attach an image file.', 400);
+    };
+
+    const profile = await CompanyProfile.findOne({ user: req.user.id });
+
+    // delete old logo from Cloundinary
+    if (profile?.logo?.publicId) {
+        await deleteFromCloudinary(profile.logo.publicId, 'image');
+    }
+
+    const updated = await CompanyProfile.findOneAndUpdate(
+        { user: req.user.id },
+        {
+            $set: {
+                'logo.url': req.file.path,
+                'logo.publicId': req.file.filename,
+            },
+        },
+        { new: true, upsert: true }
+    );
+
+    return sendSuccess(res, {
+        message: 'Logo uploaded successfully.',
+        data: { logo: updated.logo },
+    });
+});
+
+// DRIVES
+
+// GET My Drives
+
+export const getMyDrives = asyncHandler(async (req, res) => {
+    const company = await getCompanyOrFail(req.user.id);
+
+    if (company.approvalStatus !== 'approved') {
+        throw new AppError(
+            `Your company account is ${company.approvalStatus}. You cannot manage drives until approved.`,
+            403
+        );
+    }
+
+    const { status } = req.query;
+    const filter = { company: company._id };
+    if (status) filter.status = status;
+
+    const totalCount = await Drive.countDocuments(filter);
+    const { skip, limit, meta } = getPagination(req.query, totalCount);
+
+    const drives = await Drive.find(filter)
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .select('-jobDescription -aiWeights'); // exclude heavy fields from list
+
+    return sendSuccess(res, {
+        message: 'Drives fetched successfully.',
+        data: { drives },
+        meta,
+    });
+});
